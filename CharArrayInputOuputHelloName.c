d236aed30afa3dbca777d5a07a46e94685847358
tulipsamiun <tsamiun_ts@yahoo.com>
2022-06-15 04:17:06 +0600

    Create CharArrayInputOuputHelloName.c

---
+++ CharArrayInputOuputHelloName.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    char name[20];
+    printf("Enter Your Name ");
+    scanf("%s", name);
+    printf("Hello %s", name);
+
+    return 0;
+}
